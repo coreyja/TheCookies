b6f5f2a30d2b6c2efe44dc4f2c1cca97b5079af9
Corey Alexander <coreyja@gmail.com>
2020-03-06 21:51:16 -0500

    Get basic expo wrapper working

---
+++ mobile/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+export default class App extends React.Component {
+  render() {
+    return <WebView source={{ uri: 'https://thecookies.herokuapp.com/' }} style={{ marginTop: 50 }} />;
+  }
+}
+
